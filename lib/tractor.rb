2b535da78e84036c0326fc8976e5350a9ddd2221
Will <will@kindleman.com.au>
2025-03-04 14:14:51 +0000

    f

--- lib/tractor.rb
+++ lib/tractor.rb
@@ -6,6 +6,9 @@ module Tractor
   class Error < StandardError; end
   # Your code goes here...
   class Engine < ::Rails::Engine
+    initializer "tractor.assets.precompile" do |app|
+      app.config.assets.precompile += %w[admin_helpers.scss]
+    end
   end
 end
 
