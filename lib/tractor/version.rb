cc527390496e871121f0754feb2dd5137a81854a
Will <will@kindleman.com.au>
2025-03-28 13:29:08 +0000

    oops

--- lib/tractor/version.rb
+++ lib/tractor/version.rb
@@ -1,32 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "tractor/menus/version"
-
 module Tractor
-  module Menus
-    class Error < StandardError; end
-    
-    class Engine < ::Rails::Engine
-      isolate_namespace Tractor::Menus
-      
-      # Add the assets paths to the engine
-      initializer "tractor-menus.assets.paths" do |app|
-        app.config.assets.paths << root.join("app", "assets", "stylesheets")
-      end
-
-      # Configure Dart Sass builds if needed
-      initializer "tractor-menus.assets.configure" do |app|
-        # Add any Dart Sass configuration here if needed
-      end
-      
-      # Load and run migrations from the gem
-      initializer :append_migrations do |app|
-        unless app.root.to_s.match(root.to_s)
-          config.paths["db/migrate"].expanded.each do |expanded_path|
-            app.config.paths["db/migrate"] << expanded_path
-          end
-        end
-      end
-    end
-  end
+  VERSION = "0.1.0"
 end
