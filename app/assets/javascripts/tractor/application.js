afa8bcecfc04c856f47c3e570702783d100a1ead
Will <will@kindleman.com.au>
2025-04-16 14:26:24 +0100

    mde

--- app/assets/javascripts/tractor/application.js
+++ app/assets/javascripts/tractor/application.js
@@ -3,16 +3,16 @@ import "controllers"
 
 console.log('tractor js');
 
-// document.addEventListener('turbo:load', loadEditors)
+document.addEventListener('turbo:load', loadEditors)
 
-// function loadEditors(event) {
-//   document.querySelectorAll('.easymde').forEach(function(mdetextarea) {
-//     console.log(mdetextarea);
-//     var editor = new EasyMDE({
-//       element: mdetextarea      
-//     }); 
-//   });
-//   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-//   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+function loadEditors(event) {
+  document.querySelectorAll('.easymde').forEach(function(mdetextarea) {
+    console.log(mdetextarea);
+    var editor = new EasyMDE({
+      element: mdetextarea      
+    }); 
+  });
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// }
+}
